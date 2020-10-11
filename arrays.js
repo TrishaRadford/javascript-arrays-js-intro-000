d2c6e3cccee2a0be43d3fe4ee5af3cdbb8401e55
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-11 21:47:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function addElementToBeginningOfArray(array, element){
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   var array = ['a', 'b', 'c', 'd'];
-  array.push('e');
+  array.unshift('e');
   console.log(array);
 }
 
